7f2cdf2948f717998372df52bb163adacda22519
Cristopher López Santana <cristopher.correo@gmail.com>
2014-03-01 10:47:17 +0000

    Tested add task to column

--- server/test/ColumnTest.py
+++ server/test/ColumnTest.py
@@ -1,15 +1,29 @@
 import unittest
+from server.test.CreateTaskTest import Task
 
 
 class Column():
   def __init__(self, title):
     self.__title = title
+    self.task_dictionary = {}
 
   def get_title(self):
     return self.__title
+
+  def add(self, task):
+    self.task_dictionary[task.get_title()] = task
+
+  def get_task(self, task):
+    return self.task_dictionary[task.get_title()]
 
 
 class ColumnTest(unittest.TestCase):
   def test_create_column(self):
     column=Column("MyTitle")
     self.assertEqual("MyTitle", column.get_title())
+
+  def test_add_task_to_column(self):
+    task = Task("MyTitle Task", "Description of the task title")
+    column = Column("MyTitle Column")
+    column.add(task)
+    self.assertEqual("MyTitle Task", column.get_task(task).get_title())
