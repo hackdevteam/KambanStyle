9b807d876b165ca6ebc3f9b380316ba2b9c1df21
Cristopher López Santana <cristopher.correo@gmail.com>
2014-03-01 10:38:04 +0000

    Fixed column creation test

--- server/test/ColumnTest.py
+++ server/test/ColumnTest.py
@@ -1,10 +1,15 @@
+import unittest
+
+
 class Column():
   def __init__(self, title):
     self.__title = title
 
+  def get_title(self):
+    return self.__title
 
-class ColumnTest:
+
+class ColumnTest(unittest.TestCase):
   def test_create_column(self):
     column=Column("MyTitle")
     self.assertEqual("MyTitle", column.get_title())
-  
