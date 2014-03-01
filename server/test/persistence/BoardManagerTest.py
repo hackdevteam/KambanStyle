3a55c2446117ec480b6e4e2c0789528b757190af
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2014-03-01 18:28:00 +0000

    defining new tests for save and load tasks

--- server/test/persistence/BoardManagerTest.py
+++ server/test/persistence/BoardManagerTest.py
@@ -21,16 +21,34 @@ class BoardManagerTest(unittest.TestCase):
 		boar_manager = BoardManager(TEST_BOARD_ID)
 		boar_manager.save_column(MockColumn("column 1"))
 		boar_manager.save_column(MockColumn("column 2"))
-		column = boar_manager.load_column("column 1")
-		self.assertEqual("column 1", column.id)
+		column = boar_manager.load_column(boar_manager.columns_ids()[0])
+		self.assertEqual("column 1", column.name)
 
 	def test_save_column(self):
 		boar_manager = BoardManager(TEST_BOARD_ID)
-		boar_manager.save_column(MockColumn("column1"))
-		boar_manager.save_column(MockColumn("column2"))
-		boar_manager.save_column(MockColumn("column3"))
+		boar_manager.save_column(MockColumn("column 1"))
+		boar_manager.save_column(MockColumn("column 2"))
+		boar_manager.save_column(MockColumn("column 3"))
 		self.assertEqual(3, len(boar_manager.columns_ids()))
 
+	def test_save_a_column_with_duplicated_name(self):
+		boar_manager = BoardManager(TEST_BOARD_ID)
+		boar_manager.save_column(MockColumn("column 1"))
+		boar_manager.save_column(MockColumn("column 1"))
+		self.assertEqual(2, len(boar_manager.columns_ids()))
+
+	def test_save_a_task(self):
+		pass
+
+	def test_save_several_tasks(self):
+		pass
+
+	def test_delete_a_task(self):
+		pass
+
+	def test_delete_a_deleted_task(self):
+		pass
+	
 	def remove_data(self):
 		data_path = os.path.abspath("../../resources/" + TEST_BOARD_ID)
 		if os.path.exists(data_path):
