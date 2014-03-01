75ba429f83110a1118f390fee94ccc78496e9800
Cristopher López Santana <cristopher.correo@gmail.com>
2014-03-01 12:00:44 +0000

    Tested remove item to column

--- server/test/ColumnTest.py
+++ server/test/ColumnTest.py
@@ -16,10 +16,16 @@ class Column():
   def get_task(self, task):
     return self.task_dictionary[task.get_title()]
 
+  def get_tasks(self):
+    return self.task_dictionary
+
+  def remove(self, task_name):
+    self.task_dictionary.__delitem__(task_name)
+
 
 class ColumnTest(unittest.TestCase):
   def test_create_column(self):
-    column=Column("MyTitle")
+    column = Column("MyTitle")
     self.assertEqual("MyTitle", column.get_title())
 
   def test_add_task_to_column(self):
@@ -27,3 +33,10 @@ class ColumnTest(unittest.TestCase):
     column = Column("MyTitle Column")
     column.add(task)
     self.assertEqual("MyTitle Task", column.get_task(task).get_title())
+
+  def test_remove_task_from_column(self):
+    task = Task("MyTitle Task", "Description of the task title")
+    column = Column("MyTitle Column")
+    column.add(task)
+    column.remove(task.get_title())
+    self.assertEqual(0, len(column.get_tasks()))
