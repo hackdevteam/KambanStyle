8b60c75e43c165aef0a8614b628ee56f7437a36f
Josue <josu.vicio@gmail.com>
2014-03-11 13:43:38 +0000

    Added task api end point

--- server/api/TaskApiEndPoint.py
+++ server/api/TaskApiEndPoint.py
@@ -1,1 +1,16 @@
+import json
+from uuid import uuid4
+from server.kamban.model.Task import Task
 
+
+class TaskApiEndPoint(object):
+    exposed = True
+
+    def __init__(self, persistence):
+        self.__persistance = persistence
+
+    def POST(self, title, description):
+        task = Task(title, description, str(uuid4()))
+        self.__persistance.save(task)
+        return json.dumps({"title": task.get_title(), "description": task.get_description(), "id": task.get_id(),
+                           "column_id": task.get_column_id()})
--- server/test/api/TaskApiTest.py
+++ server/test/api/TaskApiTest.py
@@ -1,9 +1,31 @@
 import unittest
+import cherrypy
+import requests
+from server.api.TaskApiEndPoint import TaskApiEndPoint
+from server.test.api.PersistenceMock import PersistenceMock
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class TaskApiTest(unittest.TestCase):
+    def setUp(self):
+        cherrypy.tree.mount(
+            TaskApiEndPoint(PersistenceMock()), '/api/task',
+            {'/':
+                 {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}
+            }
+        )
+        cherrypy.engine.start()
+
+
+    def tearDown(self):
+        cherrypy.engine.stop()
+        cherrypy.engine.exit()
+    
+
+    def test_create_task(self):
+        response = requests.post('http://localhost:8080/api/task',
+                                 {'title': 'Thing to do', 'description': 'Important thing to do'})
+        self.assertEqual(200, response.status_code)
+        self.assertIn('Thing to do', response.text)
 
 
 if __name__ == '__main__':
