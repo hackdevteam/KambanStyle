57ec56b1e966abd71f3027db59567e1c1fce2b33
Víctor Pérez Santana <crakocia@gmail.com>
2014-03-18 12:27:44 +0000

    Modify templates, add post calls in main.js

--- client/js/kanbanstyle/main.js
+++ client/js/kanbanstyle/main.js
@@ -17,14 +17,49 @@ function Main() {
     this.initJquery = new function() {
         $(function() {    
             this.getBoard;
+            this.getColumn;
+            this.getTask;
         });
     };
 
     this.getBoard = new function() {
-        $.post("localhost/api/board",
-        {title: "nuevo"},
-        function (response){
-           $('#template').tmpl(response).appendTo('body');
+        $.post("board.php",
+        {title: "NewBoard"},
+        function (response){           
+           $('#templateBoard').tmpl(response).appendTo('body');
+        },
+        "json");
+    };
+    
+    this.getColumn = new function() {
+        $.post("column.php",
+        {idb : "001", title: "FirtsColumn"},
+        function (response){          
+           var id = response.idc;
+           $('#templateColumn').tmpl(response).appendTo('#column-area');
+        },
+        "json");
+        
+        $.post("column.php",
+        {idb : "001", title: "SecondColumn"},
+        function (response){           
+           $('#templateColumn').tmpl(response).appendTo('#column-area');
+        },
+        "json");
+    };
+    
+     this.getTask = new function() {
+        $.post("task.php",
+        {idc : "001c", title: "FirtsTask", description : "Primera Tarea"},
+        function (response){  
+           $('#templateTask').tmpl(response).appendTo('#'+response.idc+'-list-task-area');
+        },
+        "json");
+        
+        $.post("task.php",
+        {idc : "001c", title: "SecondColumn", description : "Segund Tarea"},
+        function (response){           
+           $('#templateTask').tmpl(response).appendTo('#'+response.idc+'-list-task-area');
         },
         "json");
     };
