11d05187bdac056ee72bb985d7e3fa19f9d22d7b
ramclen <ramclenlp@gmail.com>
2014-03-19 14:36:38 +0000

    ColumnDataMapperTest created and passed

--- server/persistence/database/ColumnDataMapper.py
+++ server/persistence/database/ColumnDataMapper.py
@@ -2,10 +2,10 @@ from server.persistence.database.DataMapper import DataMapper
 
 
 class ColumnDataMapper(DataMapper):
-  def __init__(self):
-    super().__init__()
+    def insert(self, column):
+        self.query = "INSERT INTO column (title, idb) VALUES(?,?)"
+        self.abstract_insert(column.get_title(), column.get_board_id())
 
-  def insert(self, column):
-    super.query = "INSERT INTO column (title, idb) VALUES(?,?,?)"
-    super().abstract_insert((column.get_title(), column.get_board_id()))
-
+    def retrieve(self, column_title):
+        self.query = "SELECT * FROM column WHERE title=?"
+        return self.abstract_retrieve(column_title)
--- server/test/persistence/database/ColumnDataMappertTest.py
+++ server/test/persistence/database/ColumnDataMappertTest.py
@@ -1,1 +1,19 @@
-__author__ = 'Maca'
+import unittest
+from server.kanban.model.Column import Column
+from server.persistence.database.ColumnDataMapper import ColumnDataMapper
+
+
+class ColumnDataMapperTest(unittest.TestCase):
+
+  def test_insert(self):
+    mapper = ColumnDataMapper()
+    mapper.insert(Column("My Column", 2))
+    self.assertEqual(1, mapper.retrieve("My Column").fetchall().__sizeof__())
+
+  def test_retrieve(self):
+      mapper = ColumnDataMapper()
+      self.assertEqual("", mapper.retrieve("FirstColumn").fetchone()[1])
+
+
+if __name__ == '__main__':
+  unittest.main()
