ba8a869304f7d54d160531618efbf6be6466cb88
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2014-03-23 20:53:36 +0000

    creating and viewing columns from the view

--- client/js/kanbanstyle/main.js
+++ client/js/kanbanstyle/main.js
@@ -1,20 +1,22 @@
-function Board() {
-    this.board_id = [];
-    this.board_title = [];
-    this.columns = [];
-}
-var board = new Board();
-
 function boardCreationResponse(response) {
-    board.board_id = JSON.parse(response).board_id;
-    board.board_title = JSON.parse(response).title;
     $('#templateBoard').tmpl(JSON.parse(response)).appendTo(".board-area");
+    createColumn("Column 1", $(".board").attr("id"));
+}
 
+function columnCreationResponse(response) {
+    console.log(JSON.parse(response));
+    $('#templateColumn').tmpl(JSON.parse(response)).appendTo(".column-area");
 }
 
 function createBoard(title) {
     $.post("api/board", {title: title}).done(function (response) {
         boardCreationResponse(response);
+    })
+}
+
+function createColumn(title, board_id) {
+    $.post("api/column", {title: title, board_id: board_id}).done(function (response) {
+        columnCreationResponse(response);
     })
 }
 
