0821a2df38b2d3152ba3bc532ab87d3916d0f4d6
Josue <josu.vicio@gmail.com>
2014-04-01 14:30:11 +0100

    Retrieved test from a missing commit in a branch

--- server/test/persistence/database/BoardDataMapperTest.py
+++ server/test/persistence/database/BoardDataMapperTest.py
@@ -4,11 +4,14 @@ from server.persistence.database.BoardDataMapper import BoardDataMapper
 
 
 class BoardDataMapperTest(unittest.TestCase):
-
     def test_insert(self):
         mapper = BoardDataMapper()
         mapper.insert(Board("My Board"))
-        self.assertEqual(2, len(mapper.retrive("My Board")))
+        self.assertEqual("My Board", mapper.retrieve("My Board").fetchone()[1])
+
+    def test_retrieve(self):
+        mapper = BoardDataMapper()
+        self.assertEqual("FirstBoard", mapper.retrieve("FirstBoard").fetchone()[1])
 
 
 if __name__ == '__main__':
