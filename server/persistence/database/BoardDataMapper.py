9cf890a8d0853a7d6826aad91d2f2886998ddd53
Josue <josu.vicio@gmail.com>
2014-04-06 10:40:41 +0100

    Fixed BoardDataMapper

--- server/persistence/database/BoardDataMapper.py
+++ server/persistence/database/BoardDataMapper.py
@@ -3,12 +3,12 @@ from server.persistence.database.DataMapper import DataMapper
 
 class BoardDataMapper(DataMapper):
     def __init__(self):
-        super.__init__()
+        DataMapper()
 
     def insert(self, board):
-        self.query = "INSERT INTO my_board (title) VALUES(?)"
+        self.query = "INSERT INTO board (title) VALUES(?)"
         self.abstract_insert(board.get_title())
 
     def retrieve(self, board_title):
-        self.query = "SELECT * FROM my_board WHERE title=\" ? \""
-        self.abstract_retrive(board_title)
+        self.query = "SELECT * FROM board WHERE title=?"
+        return self.abstract_retrieve(board_title)
