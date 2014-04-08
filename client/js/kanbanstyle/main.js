48bd89f428c4bac10de8751e9115e36f17867eb5
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2014-04-08 14:20:39 +0100

    refactored to create the two objects, Context and Connection

--- client/js/kanbanstyle/main.js
+++ client/js/kanbanstyle/main.js
@@ -1,63 +1,43 @@
-function boardCreationResponse(response) {
-    $('#templateBoard').tmpl(JSON.parse(response)).appendTo(".board-area");
+function Connection() {
+    this.boardCreationResponse = function (response) {
+        $('#templateBoard').tmpl(JSON.parse(response)).appendTo(".board-area");
+    };
+
+    this.columnCreationResponse = function(response) {
+        $('#templateColumn').tmpl(JSON.parse(response)).appendTo(".column-area");
+    };
+
+    this.taskCreationResponse = function(response) {
+        var jsonResponse = JSON.parse(response);
+        $('#templateTask').tmpl(jsonResponse).appendTo("#" + jsonResponse.column_id + "-task-area");
+    };
+
+    this.createBoard = function(title) {
+        $.post("api/board", {title: title}).done(function (response) {
+            boardCreationResponse(response);
+        })
+    };
+
+    this.createColumn = function(title, board_id) {
+        $.post("api/column", {title: title, board_id: board_id}).done(function (response) {
+            columnCreationResponse(response);
+        })
+    };
+
+    this.createTask = function(title, description, column_id) {
+        $.post("api/task", {title: title, description: description, column_id: column_id}).done(function (response) {
+            taskCreationResponse(response);
+        })
+    };
+}
+function Context(){
+    this.boardId = [];
+    this.columns = [];
+    this.getBoardId = function(){return this.boardId;};
+    this.setBoardId = function(boardId){this.boardId = boardId;};
+    this.getNumberOfColums = function(){return this.columns.length};
 }
 
-function columnCreationResponse(response) {
-    $('#templateColumn').tmpl(JSON.parse(response)).appendTo(".column-area");
-}
+var connection = new Connection();
+var context = new Context();
 
-function taskCreationResponse(response) {
-    var jsonResponse = JSON.parse(response);
-    $('#templateTask').tmpl(jsonResponse).appendTo("#" + jsonResponse.column_id + "-task-area");
-}
-
-function createBoard(title) {
-    $.post("api/board", {title: title}).done(function (response) {
-        boardCreationResponse(response);
-    })
-}
-
-function createColumn(title, board_id) {
-    $.post("api/column", {title: title, board_id: board_id}).done(function (response) {
-        columnCreationResponse(response);
-    })
-}
-
-function createTask(title, description, column_id) {
-    $.post("api/task", {title: title, description: description, column_id: column_id}).done(function (response) {
-        taskCreationResponse(response);
-    })
-}
-
-
-createBoard("Board 1");
-setTimeout(function () {
-        createColumn("Column 1", $(".board").attr("id"));
-    }
-    , 100);
-
-setTimeout(function () {
-        createColumn("Column 2", $(".board").attr("id"));
-    }
-    , 200);
-
-setTimeout(function () {
-        createTask("task 1", "description", $(".column").map(function () {
-            return $(this).attr("id");
-        }).get()[0]);
-    }
-    , 600);
-
-setTimeout(function () {
-        createTask("task 2", "description", $(".column").map(function () {
-            return $(this).attr("id");
-        }).get()[0]);
-    }
-    , 600);
-
-setTimeout(function () {
-        createTask("task 3", "description", $(".column").map(function () {
-            return $(this).attr("id");
-        }).get()[1]);
-    }
-    , 600);
