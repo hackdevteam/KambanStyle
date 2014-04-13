80852765e3f7ac2a6f3563512c688d39c4d2400e
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2014-04-13 15:03:47 +0100

    added files to test client with jasmine

--- client/js/kanbanstyle/main.js
+++ client/js/kanbanstyle/main.js
@@ -1,52 +1,58 @@
-$(document).ready(function () {
-    $('#templateCreateBoardForm').tmpl({id: "createBoardForm", name: "Create a new Board"}).appendTo("body");
+$(document).ready(function(){
+    //$('#templateCreateBoardForm').tmpl({id: "createBoardForm", name: "Create a new Board"}).appendTo("body");
 });
 
-function Responses() {
-    this.boardCreationResponse = function (response) {
+function PresentationManager(){
+    this.createBoard = function(boardData, boardArea){
+        boardArea.html($('#templateBoard').tmpl(boardData));
+    };
+}
+
+function Responses(){
+    this.boardCreationResponse = function(response){
         $("body").html($('#templateBoard').tmpl(JSON.parse(response)));
     };
 
-    this.columnCreationResponse = function (response) {
+    this.columnCreationResponse = function(response){
         $('#templateColumn').tmpl(JSON.parse(response)).appendTo(".column-area");
     };
 
-    this.taskCreationResponse = function (response) {
+    this.taskCreationResponse = function(response){
         var jsonResponse = JSON.parse(response);
         $('#templateTask').tmpl(jsonResponse).appendTo("#" + jsonResponse.column_id + "-task-area");
     };
 }
 
-function Connection() {
-    this.createBoard = function (title) {
-        $.post("api/board", {title: title}).done(function (response) {
+function Connection(){
+    this.createBoard = function(title){
+        $.post("api/board", {title: title}).done(function(response){
             new Responses().boardCreationResponse(response);
         })
     };
 
-    this.createColumn = function (title, board_id) {
-        $.post("api/column", {title: title, board_id: board_id}).done(function (response) {
+    this.createColumn = function(title, board_id){
+        $.post("api/column", {title: title, board_id: board_id}).done(function(response){
             new Responses().columnCreationResponse(response);
         })
     };
 
-    this.createTask = function (title, description, column_id) {
-        $.post("api/task", {title: title, description: description, column_id: column_id}).done(function (response) {
+    this.createTask = function(title, description, column_id){
+        $.post("api/task", {title: title, description: description, column_id: column_id}).done(function(response){
             new Responses().taskCreationResponse(response);
         })
     };
 }
-function Context() {
+function Context(){
     this.boardId = [];
     this.boardTitle = [];
     this.columns = [];
-    this.getBoardId = function () {
+    this.getBoardId = function(){
         return this.boardId;
     };
-    this.setBoardId = function (boardId) {
+    this.setBoardId = function(boardId){
         this.boardId = boardId;
     };
-    this.getNumberOfColums = function () {
+    this.getNumberOfColums = function(){
         return this.columns.length
     };
 }
@@ -54,11 +60,11 @@ function Context() {
 var connection = new Connection();
 var context = new Context();
 
-function createBoard() {
+function createBoard(){
     var formObj = {};
     var inputs = $("#formCreateBoard").serializeArray();
-    $.each(inputs, function (i, input) {
+    $.each(inputs, function(i, input){
         formObj[input.name] = input.value;
     });
-    connection.createBoard(formObj["nameBoard"]);
+    connection.createBoard(formObj["nameBoard"], boardArea);
 }
--- client/test/spec/basicSpec.js
+++ client/test/spec/basicSpec.js
@@ -1,5 +1,17 @@
-describe("A suite", function() {
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
-  });
+describe("Double click allows to edit", function(){
+    beforeEach(function(){
+        $(".html-reporter").before("<section class='board-area'></section>");
+    });
+
+    describe("double click on board title", function(){
+        it("should change the board title label for a text box", function(){
+            var presentationManager = new PresentationManager();
+            var boardArea = $(".board-area");
+            var boardId = "brdtest";
+            var boardTitle = "TestBoard";
+            var boardData = {board_id: boardId, title: boardTitle};
+            presentationManager.createBoard(boardData, boardArea);
+            expect($("#"+boardId)).toBeInDOM();
+        });
+    });
 });
