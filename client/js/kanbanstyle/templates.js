c821a0af811bb2dc197df1cafae9c89d4a52583f
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2014-04-13 17:49:25 +0100

    moved the column template to templates.js

--- client/js/kanbanstyle/templates.js
+++ client/js/kanbanstyle/templates.js
@@ -1,12 +1,21 @@
-var boardTemplate = $(
-    "<div>"+
+var boardTemplate =$(
+    "<div>" +
         "<div class='board' id='${board_id}'>" +
-            "<div id='title'>" +
-                "<h1>${title}</h1>" +
-                "<button id='createColumnButton'>Create Column</button>" +
+            "<div class='board-title-area'>" +
+                "<h1 id='board-title'>${title}</h1>" +
             "</div>" +
-            "<div>" +
-                "<div class='column-area'></div>" +
-            "</div>" +
+            "<button id='createColumnButton'>Create Column</button>" +
+            "<div class='column-area'></div>" +
         "</div>" +
     "</div>");
+
+var columnTemplate = $(
+    "<div>" +
+        "<div class='column' id='${column_id}'>" +
+            "<h1>${title}</h1>" +
+            "<div id='${column_id}-task-area' class='list-task'></div>" +
+        "</div>" +
+    "</div>");
+
+
+var editBoardTitleTemplate = $("<form id='edit-board-title' action='javascript:modifyBoardTitle()'><input type='text'></form>");
