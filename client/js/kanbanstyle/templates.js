55d4ce5ee6976efbfa6cc3f25c17c010cb562f17
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2014-04-14 10:45:46 +0100

    moved the task template from the index to templates.js and moved the cereate board form to the body

--- client/js/kanbanstyle/templates.js
+++ client/js/kanbanstyle/templates.js
@@ -4,7 +4,7 @@ var boardTemplate =$(
             "<div class='board-title-area'>" +
                 "<h1 id='board-title'>${title}</h1>" +
             "</div>" +
-            "<button id='createColumnButton'>Create Column</button>" +
+            "<button id='create-column-button'>Create Column</button>" +
             "<div class='column-area'></div>" +
         "</div>" +
     "</div>");
@@ -13,9 +13,18 @@ var columnTemplate = $(
     "<div>" +
         "<div class='column' id='${column_id}'>" +
             "<h1>${title}</h1>" +
-            "<div id='${column_id}-task-area' class='list-task'></div>" +
+            "<div class='task-area'></div>" +
         "</div>" +
     "</div>");
 
+var taskTemplate = $(
+    "<div>" +
+        "<div id=${task_id} class='task'>" +
+            "<h2>${title}</h2>" +
+            "<p>${description}</p>" +
+        "</div>" +
+    "</div>");
 
 var editBoardTitleTemplate = $("<form id='edit-board-title' action='javascript:modifyBoardTitle()'><input type='text'></form>");
+var editColumnTitleTemplate = $("<form id='edit-column-title' action='javascript:modifyColumnTitle()'><input type='text'></form>");
+var editTaskTitleTemplate = $("<form id='edit-task-title' action='javascript:modifyTaskTitle()'><input type='text'></form>");
