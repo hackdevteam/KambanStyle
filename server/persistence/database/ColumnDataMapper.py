cfb8ebf78b47e17db59e9f7c11c1e096c52e1053
Josue <josu.vicio@gmail.com>
2014-04-21 18:41:37 +0100

    Modified retrieve to return id in ColumnDataMapper

--- server/persistence/database/ColumnDataMapper.py
+++ server/persistence/database/ColumnDataMapper.py
@@ -10,9 +10,9 @@ class ColumnDataMapper(DataMapper):
         self.query = "INSERT INTO column (title, idb) VALUES(?,?)"
         return self.abstract_insert([column.get_title(), column.get_board_id()])
 
-    def retrieve(self, column_title):
-        self.query = "SELECT * FROM column WHERE title=?"
-        column_tuple = self.abstract_retrieve([column_title, ]).fetchone()
+    def retrieve(self, idc):
+        self.query = "SELECT * FROM column WHERE idc=?"
+        column_tuple = self.abstract_retrieve([idc, ]).fetchone()
         return Column(column_tuple[1], column_tuple[2])
 
     def update(self, title, idb, idc):
